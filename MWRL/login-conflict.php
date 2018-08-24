1b18004eb4865db4e662622f83b29aa4b7daaa02
WuYuanhun <30903332+WuYuanhun@users.noreply.github.com>
2018-08-24 09:29:46 +0800

    #15 fix conflict

--- MWRL/login-conflict.php
+++ MWRL/login-conflict.php
@@ -1,10 +1,49 @@
-<<<<<<< HEAD:MWRL/login.php
 <?php
-  $targetUrl = $_GET['redUrl'];
-  $username = $_GET['username'];
-  $password = $_GET['password'];
-  
+ function openJason($path)
+ {
+     if(file_exists($path)==false)
+     {
+        echo '<script>alert("Unknown Error! Connect the admin");
+            location.href="http://39.104.90.230/5308/"</script>';
+     }
+     else
+     {
+        $s = file_get_contents($path);
+        $json = json_decode($s, true);
+     }
+     return $json;
+ }
+
+ function sCookie($usrName,$usrKey, $random)
+ {
+   setcookie("userName", $usrName, time()+3600*24*30);
+   setcookie("userKey", $usrKey, time()+3600*24*30);
+   setcookie("CC", $random, time()+3600*24*30);
+ }
+
 ?>
+
+<?php
+                if($_COOKIE[''])
+                if($_POST['submit'])
+                {
+                  $usrname = $_POST['usrname'];
+                  $password = $_POST['password'];
+                  if($usrname == '' || $usrname == ' ' )
+                  {
+                        $usrname = null;
+                  }
+                
+                  $PASpath='./password.json';
+                  
+                  $json = openJason($PASpath);
+                  if($json[$usrname] == $password)
+                  {
+                    $setR = rand(0,255) + rand(0,255) + rand(0,255);
+                    sCookie($usrname, md5(md5($password) + md5($setR)), $setR);
+                  }
+                }
+                ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -37,10 +76,10 @@
               
                   <form method="post" class="am-form">
                     <label for="email">邮箱:</label>
-                    <input type="email" name="" id="email" value="">
+                    <input type="email" name="usrname" id="email" value="">
                     <br>
                     <label for="password">密码:</label>
-                    <input type="password" name="" id="password" value="">
+                    <input type="password" name="password" id="password" value="">
                     <br>
                     <label for="remember-me">
                       <input id="remember-me" type="checkbox">
@@ -48,12 +87,13 @@
                     </label>
                     <br />
                     <div class="am-cf">
-                      <input type="submit" name="" value="登 录" class="am-btn am-btn-primary am-btn-sm am-fl">
+                      <input type="submit" name="submit" value="登 录" class="am-btn am-btn-primary am-btn-sm am-fl">
                       <input type="submit" name="" value="忘记密码" class="am-btn am-btn-default am-btn-sm am-fr">
                     </div>
                   </form>
                 </div>
               </div>
+              
 
     
     <script src="assets/js/jquery.min.js"></script>
